39ddaa7c92691f60c29dbaebf1c518664fb6db2a
Premkumarm2002 <maheswarlapremkumar@gmail.com>
2024-01-11 12:41:09 +0530

    added CalciDriver to the file

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+import java.util.*;
 interface Calci
 {
 	public int getSum(int a,int b);
@@ -10,5 +11,17 @@ class CalciImple implements Calci{
 	}
 	public int getDiff(int a,int b){
 		return a-b;
+	}
+}
+class CalciDriver{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number 1:");
+		int a=sc.nextInt();
+		System.out.println("Enter the number 2:");
+		int b=sc.nextInt();
+		CalciImple c=new CalciImple();
+		System.out.println(c.getSum(a,b));
+		System.out.println(c.getDiff(a,b));
 	}
 }

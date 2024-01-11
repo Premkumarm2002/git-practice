eb9d55cde194517e5c879461dfde0a2c44d86da1
Premkumarm2002 <maheswarlapremkumar@gmail.com>
2024-01-11 12:13:49 +0530

    implementations provided for Calci

--- Calci.java
+++ Calci.java
@@ -3,3 +3,12 @@ interface Calci
 	public int getSum(int a,int b);
 	public int getDiff(int a,int b);
 }
+
+class CalciImple implements Calci{
+	public int getSum(int a,int b){
+		return a+b;
+	}
+	public int getDiff(int a,int b){
+		return a-b;
+	}
+}
